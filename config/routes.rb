ed5205183c23a6b4c64306e5d6413ad8daee6937
Nicholas Scott <nickscott247@btopenworld.com>
2013-03-30 16:43:30 +0000

    Removed nestoria query scaffolding. Added nestoria query model.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
-HappyhouseV2::Application.routes.draw do
-  resources :nestoria_queries
-
-  # The priority is based upon order of creation:
+HappyhouseV2::Application.routes.draw do  # The priority is based upon order of creation:
   # first created -> highest priority.
 
   # Sample of regular route:
