a135fcb7910bef79334dcd791c2183f7ad5a64fd
Nicholas Scott <nickscott247@btopenworld.com>
2013-03-30 17:32:19 +0000

    Made NestoriaQuery an ActiveModel.

--- app/models/nestoria_query.rb
+++ app/models/nestoria_query.rb
@@ -1,4 +1,12 @@
 class NestoriaQuery
-  attr_accessor encoding, listing_type, place_name, price_min, price_max, bedroom_min, bedroom_max, has_photo,
-                number_of_results, page
+  include ActiveModel::Validations
+  include ActiveModel::Conversion
+  extend ActiveModel::Naming
+
+  attr_accessor :encoding, :listing_type, :place_name, :price_min, :price_max, :bedroom_min, :bedroom_max, :has_photo,
+                :number_of_results, :page
+
+  def persisted?
+    false
+  end
 end
