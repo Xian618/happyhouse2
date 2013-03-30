f28bb8c0da3bf31b2d6ad3ceb7883b816de39891
Alex Mackay <alex.mackay618@gmail.com>
2013-03-30 21:31:02 +0000

    Make NestoriaQuery.search return the result of the http get request

--- app/models/nestoria_query.rb
+++ app/models/nestoria_query.rb
@@ -1,7 +1,10 @@
 class NestoriaQuery
+  require 'net/http'
+  include HttpParamsGet
   include ActiveModel::Validations
   include ActiveModel::Conversion
-  extend ActiveModel::Naming
+  #extend ActiveModel::Naming
+  
 
   attr_accessor :encoding, :listing_type, :place_name, :price_min, :price_max, :bedroom_min, :bedroom_max, :has_photo,
                 :number_of_results, :page, :country, :pretty, :action
@@ -25,19 +28,8 @@ class NestoriaQuery
     domain='http://api.nestoria.co.uk'
     path='/api'
     hash=to_hash()
-    return http_get(domain, path, hash)
-
-	#url_string='http://api.nestoria.co.uk/api?country=uk&pretty=1&action=search_listings'
-	#url_string << '&encoding=' << @encoding
-
-	#url = URI.parse('http://<site_name>/accounts/1.json')
-	#req = Net::HTTP::Get.new(url.path)
-	#res = Net::HTTP.start(url.host, url.port) {|http|
-	#  http.request(req)
-	#}
-	#
-	#@output = JSON(res.body)["account"]
-
+    uri = URI(buildgetrequest(domain, path, hash))
+    return Net::HTTP.get(uri)
   end
 
   def to_hash
@@ -47,21 +39,6 @@ class NestoriaQuery
     }
     return hash
   end
-
-  def http_get(domain,path,params)
-    path_section="#{path}?"
-
-    all_params=params.collect { |k,v| 
-      "#{k}=#{CGI::escape(v.to_s)}".delete "@"
-    }
-
-    joined_params=all_params.join('&')
-
-    return path_section.concat(joined_params)
-
-    #return Net::HTTP.get(domain, "#{path}?".concat(params.collect { |k,v| "#{k}=#{CGI::escape(v.to_s)}" }.join('&'))) if not params.nil?
-    #return Net::HTTP.get(domain, path)
-  end  
 
   def persisted?
     false
