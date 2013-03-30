2caf0f71c98bdb32ea7efba3d20d274b3000f5b7
Alex Mackay <alex.mackay618@gmail.com>
2013-03-30 21:30:16 +0000

    Add module to build http get requests

---
+++ app/helpers/http_params_get.rb
@@ -0,0 +1,13 @@
+module HttpParamsGet
+  def buildgetrequest(domain, path, params)
+    path_section="#{path}?"
+
+    all_params=params.collect { |k,v| 
+      "#{k}=#{CGI::escape(v.to_s)}".delete "@"
+    }
+    
+    joined_params=all_params.join('&')
+
+	return domain.concat(path_section.concat(joined_params))
+  end
+end
