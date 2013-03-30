a1ee71e1ca7610c00a26a6d7261c71956291be11
Alex Mackay <alex.mackay618@gmail.com>
2013-03-30 15:59:39 +0000

    Initial commit - default rails template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HappyhouseV2::Application.config.secret_token = '08d043c242e7f3e1291d9b7b1524127dcc5f11a5b8600622708563132d3c24b23ae3210bbf1462f5eac1ed292da7439eb23d6524fe34bbad223ab566f647cb3b'
