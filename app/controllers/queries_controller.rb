62d86e7cf19e7b8f5e19c7e1b67443bad2a521d3
Nicholas Scott <nickscott247@btopenworld.com>
2013-03-31 18:29:15 +0100

    Clean up queries controll create method.

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -10,15 +10,20 @@ class QueriesController < ApplicationController
   def create
     @query = Query.new(params[:query])
     if(@query.valid?)
-      nestoria_query = NestoriaQuery.new(@query)
-      res = nestoria_query.search()
-      if(res['response']['application_response_code'].to_i < 200)
-        render :json => JSON.pretty_generate(res)
-      else
-        @query.errors.add("place_name", "Place name is ambiguous or misspelled")
-        render action: "index"
-      end
+      search_nestoria(@query)
     else
+      render action: "index"
+    end
+  end
+
+  private
+  def search_nestoria(query)
+    nestoria_query = NestoriaQuery.new(query)
+    res = nestoria_query.search()
+    if(res['response']['application_response_code'].to_i < 200)
+      render :json => JSON.pretty_generate(res)
+    else
+      query.errors.add("place_name", "Place name is ambiguous or misspelled")
       render action: "index"
     end
   end
