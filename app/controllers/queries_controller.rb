dca76f6b3df7213d5ee3d3ec16d1ade5f3a08a17
Alex Mackay <alex.mackay618@gmail.com>
2013-03-31 00:05:02 +0000

    Add error message for invalid responses from nestoria

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -12,7 +12,12 @@ class QueriesController < ApplicationController
     if(@query.valid?)
       @nestoria_query = NestoriaQuery.new(@query)
       res = @nestoria_query.search()
-      render :json => JSON.pretty_generate(res)
+      if(res['response']['response_action_code'] == 100)
+        render :json => JSON.pretty_generate(res)
+      else
+        @query.errors.add("place_name", "Place name is not specific enough")
+        render action: "index"
+      end
     else
       render action: "index"
     end
